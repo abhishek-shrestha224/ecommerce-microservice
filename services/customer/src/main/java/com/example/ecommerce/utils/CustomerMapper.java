adb0a0d5b84e0da2078ee52747794c0be86db3ba
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-28 19:34:02 +0545

    log in mapper

--- services/customer/src/main/java/com/example/ecommerce/utils/CustomerMapper.java
+++ services/customer/src/main/java/com/example/ecommerce/utils/CustomerMapper.java
@@ -2,11 +2,19 @@ package com.example.ecommerce.utils;
 
 import com.example.ecommerce.domain.CustomerDocument;
 import com.example.ecommerce.domain.CustomerRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class CustomerMapper {
   public CustomerDocument toDocument(CustomerRequest request) {
+    log.info("CustomerRequest -> CustomerDocument, {}", request);
+
+    if (null == request) {
+      log.info("CustomerRequest: NullPointerException");
+      return null;
+    }
     return CustomerDocument.builder()
         .id(null)
         .fName(request.fName())
