a40805651d30c68e41ed91898202623ad58cc023
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-28 20:59:39 +0545

    ...

--- services/customer/src/main/java/com/example/ecommerce/domain/CustomerRequest.java
+++ services/customer/src/main/java/com/example/ecommerce/domain/CustomerRequest.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.domain;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,4 +13,4 @@ public record CustomerRequest(
     @NotBlank(message = "fName is required.") String fName,
     @NotBlank(message = "lName is required.") String lName,
     @NotNull(message = "Email is required.") @Email(message = "Email is not valid.") String email,
-    @NotNull(message = "Address is required.") Address address) {}
+    @NotNull(message = "Address is required.") @Valid Address address) {}
