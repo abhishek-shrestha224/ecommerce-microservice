eef49a671eaf3a6d593a77d7553560cabab718e0
Abhishek Shrestha <meabhi224@gmail.com>
2025-03-04 11:52:16 +0545

    fixed typo in migration in product-service

--- services/product/src/main/java/com/example/ecommerce/controller/ProductController.java
+++ services/product/src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -1,4 +1,21 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.request.ProductRequest;
+import com.example.ecommerce.response.ProductResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+@Slf4j
 public class ProductController {
+
+  @PostMapping
+  public ProductResponse create(@RequestBody @Valid final ProductRequest request) {
+    return null;
+  }
 }
